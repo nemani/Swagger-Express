5f02a917df471b83a18f837a79148acbfa4b64b8
Arjun Nemani <nemaniarjun@gmail.com>
2018-10-19 16:33:48 +0530

    Instead of returning the base64, I now save the image to a folder named "imagestore" and return the path to the uploaded file

--- api/controllers/image.js
+++ api/controllers/image.js
@@ -1,14 +1,37 @@
-'use strict';
-var util = require('util');
+"use strict";
+var util = require("util");
+var fs = require("fs");
+var crypto = require("crypto");
+
 module.exports = {
-  uploadFile: uploadFile
+	uploadFile: uploadFile
 };
 
 function uploadFile(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var file = req.swagger.params.file.value;
-  var base64img = new Buffer(file.buffer).toString('base64')
-  var message = "This is the base64 encoded version of the string"
-  // this sends back a JSON response which is a single string
-  res.status(200).json({message, base64img})
+	// variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+	var file = req.swagger.params.file.value;
+
+	// Create Binary data for file
+	var imagedata = new Buffer(file.buffer).toString("binary");
+
+	// Create SHA-256 hash of the image
+	var sha256img = crypto.createHash("sha256");
+	sha256img.update(imagedata);
+	sha256img = sha256img.digest("hex");
+
+	// Get the extension
+	var ext = file.originalname.split(".").splice(-1, 1);
+
+	// calculate fpath
+	var fpath = "./imagestore/" + sha256img + "." + ext;
+
+	// Save the image
+	fs.writeFile(fpath, imagedata, "binary", function(err) {
+		if (err) throw err;
+		console.log("File saved.");
+	});
+
+	var message = "Image Saved!";
+	// this sends back a JSON response which is a single string
+	res.status(200).json({ message, fpath });
 }
